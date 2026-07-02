0d393cc077be89eab8f9811abbc4b86d545405f7
vinuthavishwanathmesta-01 <2503509@cognizant.com>
2026-07-02 10:04:27 +0530

    Added quality service implementation

---
+++ smpps-microservices/quality-service/src/test/java/com/example/quality_service/QualityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quality_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
